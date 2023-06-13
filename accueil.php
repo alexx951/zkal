3e94aaaa4fa2c3e14d3e116c7f38e21207d3ef19
alexx951 <136070677+alexx951@users.noreply.github.com>
2023-06-13 10:45:56 +0200

    accueille.01

---
+++ accueil.php
@@ -0,0 +1,29 @@
+
+<!doctype html>
+<html>
+
+<head>
+<title>Quizzo</title>
+<meta charst ="utf-8">
+<meta name= "viewprt" content ="width=device-width, initial-scale=1.0" />
+
+
+</head>
+
+<body> 
+<h1 class="text-4xl tracking-wider neon"> QUIZZO
+
+    <style>
+        .neon{
+        color: #fff;
+        text-shadow: 0 0 5px #fff, 0 0 20px #fff, 0 0 40px, #0ff, 0 0 80px #0ff, 0 0 90px #0ff;
+        }
+        </style>
+</h1>
+<div class="connexion ">
+    
+    
+
+
+</div>
+
